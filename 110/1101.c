c16ae0f14569afcee84b11b3d5ba5d17ff2da9c5
Safoun <safoun.official@gmail.com>
2026-04-17 14:55:34 +0600

    updated

--- 110/1101.c
+++ 110/1101.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
 #include <stdbool.h>
 
 int main()
 {
-    
+    while (true)
+    {
+        int a, b;
+        scanf("%d %d", &a, &b);
+
+        if (a <= 0 || b <= 0)
+        {
+            return 0;
+        }
+
+        int larger = (a > b) ? a : b;
+        int smaller = (a < b) ? a : b;
+        int sum = 0;
+
+        for (int i = smaller; i <= larger; i++)
+        {
+            printf("%d ", i);
+            sum += i;
+        }
+        printf("Sum=%d\n", sum);
+    }
+
     return 0;
 }
